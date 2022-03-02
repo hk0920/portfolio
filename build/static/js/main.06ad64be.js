7f2c34a00630ba59a682879ca0df86e7f1550155
hk0920 <gmlrb920@naver.com>
2022-03-02 22:36:23 +0900

    detail page- git url modify file upload

--- build/static/js/main.06ad64be.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ecba4f6bb3afbad201d6f019889c1b66f915abd943381a4be2f508b76e352783
-size 341035
--- src/work/Detail.js
+++ src/work/Detail.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff9ef50c233b1bc927ff92109f94727c7b89ed76bd994e8703a52d03d32f9502
-size 2199
+oid sha256:546830c860dde76587293346c5029a58d4a67fa6c65c3fe6b5bdc4e8255dec80
+size 2207
