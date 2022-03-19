77e7b7dc87f4081c05f47499fc9aad6178cf879e
hk0920 <gmlrb920@naver.com>
2022-03-19 13:28:29 +0900

    about section text modify file - build file upload

---
+++ build/static/js/main.1d07bb08.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5589dc8c7c84989a2205628baa3f94901e6cb26a0d1ff79fa66b9dbcf7a7b606
+size 341039
--- build/static/js/main.51763566.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:017de17b78610927fe8ac2344f9f16cd71cdca093f7c4e3d796f57fe2809c5ce
-size 341038
