2c2858dda452a411b907a65ed5214e8add66d8e2
hk0920 <gmlrb920@naver.com>
2022-03-19 13:25:40 +0900

    about section - text modify file upload

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0473cb2cff9caa4e58a1055e36f106c09164cb14a93ad04e2688c8734b7cdcc1
-size 9944
+oid sha256:048cb4799588ab8c8d433c1fbcf6407c9e6bc01d516887fa2e8add8334df22da
+size 9945
