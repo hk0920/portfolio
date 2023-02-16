a90009b352fa07a9e82ddfa4ded812fee591551d
hk0920 <gmlrb920@naver.com>
2023-02-16 16:05:28 +0900

    [add] loading components

--- src/components/Laoding.js
+++ src/components/Laoding.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ba8329f5ec99704fbded43b581402a92b8e2613d937377215a065cf5e448bcb
-size 1277
+oid sha256:9304d878775bbb48b0115630c0ac07746a406eff1fa1f57f3c6ff97d845c2140
+size 325
